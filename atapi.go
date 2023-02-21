8883977e2c5ad458dd86d3861d04ae8b0cb738ee
Rolly Maulana  Awangga <rolly@awang.ga>
2023-02-21 13:22:01 +0700

    RequestStructWithToken

--- atapi.go
+++ atapi.go
@@ -7,13 +7,62 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/google/go-querystring/query"
 )
 
-func PostStruct(structname interface{}, url string) (result interface{}) {
+type Token struct {
+	Key    string
+	Values string
+}
+
+func RequestStructWithToken(method string, token Token, structname interface{}, urltarget string) (result interface{}) {
+	client := http.Client{}
+	data, _ := query.Values(structname)
+	req, err := http.NewRequest(method, urltarget, strings.NewReader(data.Encode()))
+	if err != nil {
+		log.Fatalf("http.NewRequest Got error %s", err.Error())
+	}
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add(token.Key, token.Values)
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatalf("client.Do(req) Error occured. Error is: %s", err.Error())
+	}
+	defer resp.Body.Close()
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error Read Data data from request.")
+	}
+	json.Unmarshal([]byte(respBody), &result)
+	return
+}
+
+func RequestStruct(method string, structname interface{}, urltarget string) (result interface{}) {
+	client := http.Client{}
+	data, _ := query.Values(structname)
+	req, err := http.NewRequest(method, urltarget, strings.NewReader(data.Encode()))
+	if err != nil {
+		log.Fatalf("http.NewRequest Got error %s", err.Error())
+	}
+	req.Header.Add("Content-Type", "application/json")
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatalf("client.Do(req) Error occured. Error is: %s", err.Error())
+	}
+	defer resp.Body.Close()
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error Read Data data from request.")
+	}
+	json.Unmarshal([]byte(respBody), &result)
+	return
+}
+
+func PostStruct(structname interface{}, urltarget string) (result interface{}) {
 	mJson, _ := json.Marshal(structname)
-	resp, err := http.Post(url, "application/json", bytes.NewBuffer(mJson))
+	resp, err := http.Post(urltarget, "application/json", bytes.NewBuffer(mJson))
 	if err != nil {
 		fmt.Println("Could not make POST request to whatsauth")
 	}
@@ -26,8 +75,8 @@ func PostStruct(structname interface{}, url string) (result interface{}) {
 	return result
 }
 
-func Get(url string) (result interface{}) {
-	resp, err := http.Get(url)
+func Get(urltarget string) (result interface{}) {
+	resp, err := http.Get(urltarget)
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -40,9 +89,9 @@ func Get(url string) (result interface{}) {
 	return result
 }
 
-func GetStruct(structname interface{}, url string) (result interface{}) {
+func GetStruct(structname interface{}, urltarget string) (result interface{}) {
 	v, _ := query.Values(structname)
-	resp, err := http.Get(url + "?" + v.Encode())
+	resp, err := http.Get(urltarget + "?" + v.Encode())
 	if err != nil {
 		log.Fatalln(err)
 	}
