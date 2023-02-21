daa6149000463a58a320f6ad9809e2bacb4aa855
Rolly Maulana  Awangga <rolly@awang.ga>
2023-02-21 13:33:09 +0700

    test

--- atapi_test.go
+++ atapi_test.go
@@ -23,3 +23,31 @@ func TestPostStruct(t *testing.T) {
 	res := PostStruct(dt, url)
 	fmt.Println("res : ", res)
 }
+
+func TestRequestStruct(t *testing.T) {
+	dt := TestApi{
+		Phone:      "+6285155476774",
+		Password:   "#P@ssw0rd",
+		FirebaseId: "123",
+		DeviceId:   "6580fb6e714844ca",
+	}
+	url := "https://sister.ulbi.ac.id/ws.php/1.0/dokumen"
+	res := RequestStruct("GET", dt, url)
+	fmt.Println("res : ", res)
+}
+
+func TestRequestStructWithToken(t *testing.T) {
+	token := Token{
+		Key:    "token",
+		Values: "dsfdsfdsfdsfdsf",
+	}
+	dt := TestApi{
+		Phone:      "+6285155476774",
+		Password:   "#P@ssw0rd",
+		FirebaseId: "123",
+		DeviceId:   "6580fb6e714844ca",
+	}
+	url := "https://sister.ulbi.ac.id/ws.php/1.0/dokumen"
+	res := RequestStructWithToken("GET", token, dt, url) // POST DELETE PUT
+	fmt.Println("res : ", res)
+}
